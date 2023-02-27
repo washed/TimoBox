ce102057b0ded02643c335150dbc7cfdfbd7e2b5
BuildTools <unconfigured@null.spigotmc.org>
2023-02-27 08:27:37 +0100

    remove ControlLoop from player

--- extension/timobox-spotify.js
+++ extension/timobox-spotify.js
@@ -35,34 +35,6 @@ function addCommandListener() {
 	});
 }
 
-function ControlLoop() {
-	$.getJSON('http://localhost:8000/commandplayer', function(data) {
-		
-		if (data.command !== "") {
-			console.log(data);
-		}
-
-		switch (data.command) {			
-			case 'startPlaylist':
-				pressButton("play-button");
-				break;
-			case 'play':
-				pressButton("control-button-playpause");
-				break;
-			case 'next':
-				pressButton("control-button-skip-forward");
-				break;
-			case 'previous':
-				pressButton("control-button-skip-back");
-				break;			
-			case 'shuffle':
-				pressButton("control-button-shuffle");
-				break;
-		}
-	});
-}
-//setInterval(ControlLoop, 500);
-
 function waitForElement(selector, callback, timeout = 15000) {
 	const start = Date.now();
 
